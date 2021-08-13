69a68071cc95f44b4b83ffc455dd42f34298061b
pdh1124 <pdw3231@naver.com>
2021-08-13 11:30:33 +0900

    테스트 코드 및 junit5 연결

--- members/src/test/java/com/javaproject/members/contoller/HelloWorldControllerTest.java
+++ members/src/test/java/com/javaproject/members/contoller/HelloWorldControllerTest.java
@@ -1,14 +1,27 @@
 package com.javaproject.members.contoller;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+//junit5 : org.junit.jupiter.api
+//junit4 : org.junit
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest //해당클래스가 스프링부트테스트 임을 표시하는 어노테이션
 class HelloWorldControllerTest {
 
+	@Autowired
+	private HelloWorldController helloWorldController;
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void helloWorld() {
+		//잘 설정 되었는지 확인하
+		System.out.println("test");
+		System.out.println(helloWorldController.helloWorld());
 	}
+	
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
 
 }
